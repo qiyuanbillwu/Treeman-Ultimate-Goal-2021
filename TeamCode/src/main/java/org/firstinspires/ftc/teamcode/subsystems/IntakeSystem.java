a59e7dc87830179bd581c819d75f4780e7829c96
zhuxu <hbzhuxu@qq.com>
2021-03-23 17:16:14 +0800

    add timer and delayed task in IntakeSystem, LaunchSystem, MecanumDrive add stop method in Robot add auto opmode sample Auto_Linear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSystem.java
@@ -3,9 +3,15 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class IntakeSystem implements DeviceInterface {
 
     public DcMotorEx collector = null;
+
+    Timer timer = new Timer();
+
 
     public void init(HardwareMap hardwareMap) {
         collector = hardwareMap.get(DcMotorEx.class, "intake");
@@ -19,6 +25,19 @@ public class IntakeSystem implements DeviceInterface {
 
     public void intake(double speed) {
         collector.setVelocity(speed);
+    }
+
+    public void intake(double speed, double timeInSecs) {
+        intake(speed);
+        timer.schedule(new TaskStop(), (long) (timeInSecs*1000));
+    }
+
+    //    public void initMotor("")
+    class TaskStop extends TimerTask {
+        @Override
+        public void run() {
+            stop();
+        }
     }
 
     public void spit(double speed) {
